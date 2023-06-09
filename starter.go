874a8aaddcf9c4641f3ca86b8376761f79d61598
Meee <mee@ironsoftware.com>
2023-06-09 21:53:29 +0700

    Create 2 Exercises

--- starter.go
+++ starter.go
@@ -2,8 +2,33 @@ package starter
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func SayHello(name string) string {
 	return fmt.Sprintf("Hello %v. Welcome!", name)
+}
+
+func OddOrEven(number int) string {
+	oddOrEven := "odd"
+	if number%2 == 0 {
+		oddOrEven = "even"
+	}
+	return fmt.Sprintf("%v is an %v number", number, oddOrEven)
+}
+
+func FizzBuzz(n int) string {
+	result := ""
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			result += "FizzBuzz"
+		} else if i%3 == 0 {
+			result += "Fizz"
+		} else if i%5 == 0 {
+			result += "Buzz"
+		} else {
+			result += strconv.Itoa(i)
+		}
+	}
+	return result
 }
